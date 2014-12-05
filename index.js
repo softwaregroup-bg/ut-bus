d79755a424089f39c679eb094b460d709cf7ac4d
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2014-12-05 11:05:13 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,146 @@
+(function(define) {define(function(require) {
+
+    var when = require('when');
+    var jsonrpc = require('multitransport-jsonrpc');
+
+    return {
+        //properties
+        id:null,
+        serverPort:null,
+        clientPort:null,
+        clients:null,
+        server:null,
+        rpc:null,
+
+        init:function() {
+            this.clients = [];
+            this.rpc = {};
+            this.server = new jsonrpc.server(new jsonrpc.transports.server.tcp(this.serverPort), {
+                reload:this.reload.bind(this)
+            });
+            if (this.clientPort) {
+                this.connect('localhost', this.clientPort);
+            }
+        },
+
+        reload:function(x, cb) {
+            var adapt = function(r, f) {
+                return function() {
+                    if (!r || !r[f] || !r[f] instanceof Function) {
+                        return when.reject(new Error('Remote method "' + f + '" not found for object "' + (r && r.id) ? r.id : r + '"'));
+                    }
+                    var args = Array.prototype.slice.call(arguments);
+                    return when.promise(function(resolve, reject) {
+                        args.push(function(err, res) {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve(res);
+                            }
+                        });
+                        r[f].apply(undefined, args);
+                    });
+                };
+            };
+
+            var rpc = this.rpc;
+            when.all(
+                when.reduce(this.clients, function(prev, cur) {
+                    prev.push(when.promise(function(resolve, reject) {
+                        cur.request('rpc.methodList', [], function(err, result) {
+                            if (!err) {
+                                if (!(result instanceof Array)) {
+                                    result = [result];
+                                }
+                                cur.register(result);
+                                result.forEach(function(fn) {
+                                    var path = fn.split('.');
+                                    var last = path.pop();
+                                    path.reduce(function(prev, current) {
+                                        return (prev[current] = {});
+                                    }, rpc)[last] = adapt(cur, fn);
+                                });
+                                resolve(result);
+                            } else {
+                                reject(err);
+                            }
+                        });
+                    }));
+                    return prev;
+                }, [])
+            ).then(function(res) {
+                cb(undefined, 'reloaded ' + this.id);
+            }.bind(this)).catch(function(err) {
+                cb(err);
+            });
+        },
+
+        connect:function(host, port, cb) {
+            var x = new jsonrpc.client(new jsonrpc.transports.client.tcp(host, port));
+            x.register('reload');
+            this.clients.push(x);
+            if (cb) {
+                cb(null, 0);
+            }
+        },
+
+        /**
+         * Push message to queue
+         *
+         * @param {object} msg object to send
+         *
+         */
+        nq:function(msg) {
+            if (this.clients[0]) {
+                return this.clients[0].enqueue(msg);
+            }
+        },
+
+        /**
+         * Register methods available to the server and notify each client to reload the server's methods
+         *
+         * @param {object} methods object containing methods to be registered
+         * @returns {promise}
+         */
+        register:function(methods) {
+            var adapt = function(self, f) {
+                return function() {
+                    var args = Array.prototype.slice.call(arguments, 0, arguments.length - 1);
+                    var callback = arguments[arguments.length - 1];
+                    when(f.apply(self, args))
+                        .then(function(result) {callback(undefined, result);})
+                        .catch(function(error) {callback(error);});
+                };
+            };
+
+            if (methods instanceof Array) {
+                methods.forEach(function(fn) {
+                    if (fn.name) {
+                        this.server.register(fn.name, adapt(null, fn));
+                    }
+                }.bind(this));
+            } else {
+                Object.keys(methods).forEach(function(key) {
+                    if (methods[key] instanceof Function) {
+                        this.server.register(key, adapt(methods, methods[key]));
+                    }
+                }.bind(this));
+            }
+            return when.all(
+                when.reduce(this.clients, function(prev, cur) {
+                    prev.push(when.promise(function(resolve, reject) {
+                        cur.reload({}, function(err, res) {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve(res);
+                            }
+                        });
+                    }));
+                    return prev;
+                }, [])
+            );
+        }
+    };
+
+});}(typeof define === 'function' && define.amd ? define : function(factory) { module.exports = factory(require); }));
---
+++ test/test.js
@@ -0,0 +1,45 @@
+var wire = require('wire');
+
+m = wire({
+    bus1:{
+        create:'ut-bus',
+        init:'init',
+        properties:{
+            serverPort:3000,
+            clientPort:3001,
+            id:'bus1'
+        }
+    },
+    bus2:{
+        create:'ut-bus',
+        init:'init',
+        properties:{
+            serverPort:3001,
+            clientPort:3000,
+            id:'bus2'
+        }
+    }
+}, {require:require});
+
+m.then(function(c) {
+    x = c;
+    fn1 = function() {c.bus2.rpc.test.m1('bus2').then(function(result) {console.log(result);});};
+    fn2 = function() {c.bus2.rpc.m2('bus2').then(function(result) {console.log(result);});};
+    fn3 = function() {c.bus1.rpc.m3('bus1').then(function(result) {console.log(result);});};
+    c.bus1.register({
+        'test.m1':function(test) { return 'bus1(test):' + test;},
+        m2:function(test) { return 'bus1:' + test;}
+    }).then(function(r) {
+        console.log(r);
+        return c.bus2.register({
+            m3:function(test) { return 'bus2:' + test;}
+        });
+    }).then(function(r) {
+        console.log(r);
+        fn1();
+        fn2();
+        fn3();
+    }).catch(function(err) {
+        console.log(err);
+    });
+});
