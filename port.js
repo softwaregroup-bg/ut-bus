c9586642a0a6e8535f0f1ff8cf848a0085eba400
Martin Georgiev <martin.georgiev@softwaregroup-bg.com>
2015-02-12 15:36:25 +0200

    fullertan mocha api testing

--- port.js
+++ port.js
@@ -55,9 +55,8 @@ Port.prototype.init = function init() {
             prev.pub[next + '.publish'] = this.publish.bind(this);
             return prev;
         }.bind(this), methods);
-        this.bus.register(methods.req, 'ports');
-        this.bus.subscribe(methods.pub, 'ports');
         this.messagePublish = this.bus.getMethod('pub', 'publish');
+        return when.all([this.bus.register(methods.req, 'ports'), this.bus.subscribe(methods.pub, 'ports')]);
     }
 };
 
@@ -234,14 +233,20 @@ Port.prototype.pipeExec = function pipeExec(exec, concurrency) {
         try {
             self.exec(chunk, function(err, result) {
                 countActive--;
-                stream.push(err ? err : result);
+                var chunkOut = err ? err : result;
+                if (chunkOut && chunk && chunk.$$ && chunk.$$.callback){
+                    (chunkOut.$$) || (chunkOut.$$ = {});
+                    chunkOut.$$.callback = chunk.$$.callback;
+                }
+                stream.push(chunkOut);
                 if (countActive + 1 === concurrency) {
                     callback(err);
                 }
             });
         } catch (e) {
             countActive--;
-            chunk._mtid = 'error';
+            (chunk.$$) || (chunk.$$ = {});
+            chunk.$$.mtid = 'error';
             chunk.error = e;
             this.push(chunk);
             if (countActive < concurrency) {
