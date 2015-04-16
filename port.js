38d84d8e0ec8ce66540f841c96169e023ef50d31
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2015-04-16 11:18:52 +0300

    fix inspection errors

--- port.js
+++ port.js
@@ -2,7 +2,7 @@ var through2 = require('through2');
 var Readable = require('readable-stream/readable');
 var when = require('when');
 
-createQueue = function queue() {
+var createQueue = function queue() {
     var q = [];
     var r = new Readable({objectMode:true});
     var forQueue = false;
@@ -34,12 +34,12 @@ function Port() {
     this.queue = null;
     this.queues = {};
 
-    this.method = function method(method) {
-        var tokens = method.split('.');
+    this.method = function method(methodName) {
+        var tokens = methodName.split('.');
         var destination = tokens.shift() || 'ut';
         var opcode = tokens.join('.') || 'request';
         return this.bus.getMethod('req', 'request', destination, opcode);
-    }
+    };
 }
 
 Port.prototype.init = function init() {
@@ -75,13 +75,13 @@ Port.prototype.stop = function stop() {
 Port.prototype.call = function call(message) {
     return when.promise(function(resolve, reject) {
         if (!message) {
-            reject(new Error('Missing message parameter'))
+            reject(new Error('Missing message parameter'));
         } else
         if (!message.$$) {
-            reject(new Error('Missing message type'))
+            reject(new Error('Missing message type'));
         } else {
             message.$$.callback = function(msg) {
-                if (msg.$$ && msg.$$.mtid && msg.$$.mtid != 'error') {
+                if (msg.$$ && msg.$$.mtid && msg.$$.mtid !== 'error') {
                     resolve(msg);
                 }else {
                     reject(msg);
@@ -89,7 +89,7 @@ Port.prototype.call = function call(message) {
             };
         }
         if (!this.queue) {
-            reject(new Error('No connection to '+this.config.id))
+            reject(new Error('No connection to ' + this.config.id));
         } else {
             this.queue.add(message);
         }
@@ -101,11 +101,13 @@ Port.prototype.publish = function publish(msg) {
     var queue;
     if (this.queue) {
         queue = this.queue;
-    } else
-    if (conId = (msg.$$ && msg.$$.conId)) {
-        queue = this.queues[conId];
     } else {
-        queue = this.queue;
+        conId = (msg.$$ && msg.$$.conId);
+        if (conId) {
+            queue = this.queues[conId];
+        } else {
+            queue = this.queue;
+        }
     }
     if (queue) {
         queue.add(msg);
@@ -146,7 +148,7 @@ Port.prototype.decode = function decode(context) {
             .catch(function(err) {
                 err = err.$$ ? err : {$$ : {errorCode: err.code, errorMessage: err.message}};
                 for (var prop in $$) {
-                    if($$.hasOwnProperty(prop)) {
+                    if ($$.hasOwnProperty(prop)) {
                         err.$$[prop] = $$[prop];
                     }
                 }
@@ -154,7 +156,7 @@ Port.prototype.decode = function decode(context) {
                 port.findCallback(context, err);
                 stream.push(err);
             })
-            .done()
+            .done();
     }
 
     function convert(stream, msg) {
@@ -173,10 +175,11 @@ Port.prototype.decode = function decode(context) {
 
         if (port.framePattern) {
             buffer = Buffer.concat([buffer, packet]);
-            var frame;
-            while (frame = port.framePattern(buffer)) {
+            var frame = port.framePattern(buffer);
+            while (frame) {
                 buffer = frame.rest;
                 convert(this, frame.data);
+                frame = port.framePattern(buffer);
             }
             callback();
         } else {
@@ -218,13 +221,13 @@ Port.prototype.encode = function encode(context) {
 
                 if (buffer) {
                     port.log.trace && port.log.trace({$$: {opcode: 'frameOut', frame: buffer}});
-                    callback(null, buffer)
+                    callback(null, buffer);
                 } else {
                     callback();
                 }
             })
-            .catch(function(err){
-                err = err.$$ ? err : {$$ : {errorCode: err.code, errorMessage: err.message}}
+            .catch(function(err) {
+                err = err.$$ ? err : {$$ : {errorCode: err.code, errorMessage: err.message}};
                 err.$$.mtid = 'error';
                 msgCallback(err);
                 callback();
@@ -254,13 +257,13 @@ Port.prototype.pipeExec = function pipeExec(exec, concurrency) {
     concurrency = concurrency || 10;
     var self = this;
     var stream = through2({objectMode:true}, function(chunk, enc, callback) {
-        countActive++;
+        countActive += 1;
 
         try {
             self.exec(chunk, function(err, result) {
-                countActive--;
+                countActive -= 1;
                 var chunkOut = err ? err : result;
-                if (chunkOut && chunk && chunk.$$ && chunk.$$.callback){
+                if (chunkOut && chunk && chunk.$$ && chunk.$$.callback) {
                     (chunkOut.$$) || (chunkOut.$$ = {});
                     chunkOut.$$.callback = chunk.$$.callback;
                 }
@@ -270,7 +273,7 @@ Port.prototype.pipeExec = function pipeExec(exec, concurrency) {
                 }
             });
         } catch (e) {
-            countActive--;
+            countActive -= 1;
             (chunk.$$) || (chunk.$$ = {});
             chunk.$$.mtid = 'error';
             chunk.error = e;
