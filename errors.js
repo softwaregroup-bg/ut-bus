107090471667addafbfea78eea23d9b9e84a1c27
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2017-11-13 13:08:44 +1000

    feat: expose getError

--- errors.js
+++ errors.js
@@ -1,11 +1,12 @@
-var create = require('ut-error').define;
-
-var Bus = create('bus');
-var UnhandledError = create('unhandledError', Bus);
+const utError = require('ut-error');
+const create = utError.define;
+const Bus = create('bus');
+const UnhandledError = create('unhandledError', Bus);
 
 module.exports = {
     bus: Bus,
     defineError: create,
+    getError: utError.get,
     timeout: create('timeout', Bus, 'Time out'),
     methodNotFound: create('methodNotFound', Bus, 'Method {method} not found'),
     destinationNotFound: create('destinationNotFound', Bus, 'Destination {destination} not found'),
