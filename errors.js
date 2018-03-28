a63b9f0fd93d2cf1ebb69cebdc5b808de89503d0
stambata <stamen.peev@softwaregroup-bg.com>
2018-03-28 11:54:24 +0300

    fix: pass logger to utError (#45)

--- errors.js
+++ errors.js
@@ -1,21 +1,26 @@
 const utError = require('ut-error');
-const create = utError.define;
-const Bus = create('bus');
-const UnhandledError = create('unhandledError', Bus);
 
-module.exports = {
-    bus: Bus,
-    defineError: create,
-    getError: utError.get,
-    timeout: create('timeout', Bus, 'Time out'),
-    methodNotFound: create('methodNotFound', Bus, 'Method {method} not found'),
-    destinationNotFound: create('destinationNotFound', Bus, 'Destination {destination} not found'),
-    missingMethod: create('missingMethod', Bus),
-    unhandledError: function($meta) {
-        var err = new UnhandledError('Unhandled Error' + ($meta.errorMessage ? ': ' + $meta.errorMessage : ''));
-        if ($meta.errorCode) {
-            err.code = $meta.errorCode;
+module.exports = (bus) => {
+    utError.init(bus);
+    const create = utError.define;
+    // error constructors
+    const Bus = create('bus');
+    const UnhandledError = create('unhandledError', Bus);
+
+    return {
+        bus: Bus,
+        defineError: create,
+        getError: utError.get,
+        timeout: create('timeout', Bus, 'Time out'),
+        methodNotFound: create('methodNotFound', Bus, 'Method {method} not found'),
+        destinationNotFound: create('destinationNotFound', Bus, 'Destination {destination} not found'),
+        missingMethod: create('missingMethod', Bus),
+        unhandledError: function($meta) {
+            var err = new UnhandledError('Unhandled Error' + ($meta.errorMessage ? ': ' + $meta.errorMessage : ''));
+            if ($meta.errorCode) {
+                err.code = $meta.errorCode;
+            }
+            return err;
         }
-        return err;
-    }
+    };
 };
