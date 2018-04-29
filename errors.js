2c865aed911cde796931a1747a63e39e7e42a78b
stambata <stamen.peev@softwaregroup-bg.com>
2018-04-30 02:51:31 +0300

    feat: add fetchErrors method

--- errors.js
+++ errors.js
@@ -5,22 +5,27 @@ module.exports = (bus) => {
     const create = utError.define;
     // error constructors
     const Bus = create('bus');
-    const UnhandledError = create('unhandledError', Bus);
+    const UnhandledError = create('unhandledError', Bus, 'Unhandled error{errorMessage}');
 
     return {
         bus: Bus,
         defineError: create,
         getError: utError.get,
+        fetchErrors: utError.fetch,
         timeout: create('timeout', Bus, 'Time out'),
         methodNotFound: create('methodNotFound', Bus, 'Method {method} not found'),
         destinationNotFound: create('destinationNotFound', Bus, 'Destination {destination} not found'),
         missingMethod: create('missingMethod', Bus),
         unhandledError: function($meta) {
-            var err = new UnhandledError('Unhandled Error' + ($meta.errorMessage ? ': ' + $meta.errorMessage : ''));
+            var context = {
+                params: {
+                    errorMessage: $meta.errorMessage ? ': ' + $meta.errorMessage : ''
+                }
+            };
             if ($meta.errorCode) {
-                err.code = $meta.errorCode;
+                context.code = $meta.errorCode;
             }
-            return err;
+            return new UnhandledError(context);
         }
     };
 };
