d369adb08b2d23779062db3806d41772cefe7a41
Kris <kristiyan.todorov@softwaregroup-bg.com>
2018-09-17 12:08:23 +0300

    fix: ut-error deprecations

--- errors.js
+++ errors.js
@@ -1,33 +1,40 @@
 const utError = require('ut-error');
-
+var errors;
 module.exports = (bus) => {
     utError.init(bus);
     const create = utError.define;
     // error constructors
-    const Bus = create('bus');
-    const UnhandledError = create('unhandledError', Bus, 'Unhandled error{errorMessage}');
+    const defineErrors = () => {
+        const Bus = create('bus', undefined, 'Bus generic');
+        const UnhandledError = create('unhandledError', Bus, 'Unhandled error{errorMessage}');
+
+        return {
+            bus: Bus,
+            timeout: create('timeout', Bus, 'Time out'),
+            methodNotFound: create('methodNotFound', Bus, 'Method {method} not found'),
+            remoteMethodNotFound: create('remoteMethodNotFound', Bus, 'Remote method not found for "{bus}"'),
+            destinationNotFound: create('destinationNotFound', Bus, 'Destination {destination} not found'),
+            missingMethod: create('missingMethod', Bus, 'Missing method'),
+            notInitialized: create('notInitialized', Bus, 'Not initialized'),
+            unhandledError: function($meta) {
+                var context = {
+                    params: {
+                        errorMessage: $meta.errorMessage ? ': ' + $meta.errorMessage : ''
+                    }
+                };
+                if ($meta.errorCode) {
+                    context.code = $meta.errorCode;
+                }
+                return new UnhandledError(context);
+            }
+        };
+    };
+    errors = errors || defineErrors();
 
     return {
-        bus: Bus,
         defineError: create,
         getError: utError.get,
         fetchErrors: utError.fetch,
-        timeout: create('timeout', Bus, 'Time out'),
-        methodNotFound: create('methodNotFound', Bus, 'Method {method} not found'),
-        remoteMethodNotFound: create('remoteMethodNotFound', Bus, 'Remote method not found for "{bus}"'),
-        destinationNotFound: create('destinationNotFound', Bus, 'Destination {destination} not found'),
-        missingMethod: create('missingMethod', Bus),
-        notInitialized: create('notInitialized', Bus),
-        unhandledError: function($meta) {
-            var context = {
-                params: {
-                    errorMessage: $meta.errorMessage ? ': ' + $meta.errorMessage : ''
-                }
-            };
-            if ($meta.errorCode) {
-                context.code = $meta.errorCode;
-            }
-            return new UnhandledError(context);
-        }
+        ...errors
     };
 };
