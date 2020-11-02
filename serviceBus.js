ed1bb4667980c0f8e2c37471dc07fe457c97ab79
stamen.peev <stamen.peev@softwaregroup-bg.com>
2020-11-02 18:49:44 +0200

    chore: rebuild

--- serviceBus.js
+++ serviceBus.js
@@ -122,6 +122,7 @@ class Bus extends Broker {
                 }
             }
             if (!fn) {
+                // don't try skipping socket if there is a gateway configured
                 if (methodName && !(bus.rpc.gateway && bus.rpc.gateway($applyMeta, methodName))) {
                     bus.canSkipSocket && (fn = bus.findMethod(bus.mapLocal, methodName, methodType));
                     fn && (unpack = true);
