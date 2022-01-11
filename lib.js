605f1157d851886307e28ecc94a7d0d43230df94
zetxx <zetxx@belogradchik.biz>
2022-01-11 13:58:52 +0200

    fix: utcore-152

--- lib.js
+++ lib.js
@@ -2,8 +2,8 @@ module.exports = {
     loginService: (() => {
         let loginCache;
 
-        return async(discovery, what = 'login') => {
-            if (!loginCache) loginCache = discovery(what);
+        return async(discovery) => {
+            if (!loginCache) loginCache = discovery('login');
             try {
                 return await loginCache;
             } catch (error) {
