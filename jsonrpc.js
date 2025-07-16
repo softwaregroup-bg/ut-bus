680f976019d51a8d3580c55f1074e49b14e7ff23
Alexander Seferinkyn <bloc4ain@gmail.com>
2025-07-17 00:47:23 +0800

    fix: support only single language

--- jsonrpc.js
+++ jsonrpc.js
@@ -117,19 +117,6 @@ async function failPreRpc(request, h, error) {
         .takeover();
 }
 
-function parseAcceptLanguageHeader(header, except) {
-    const parts = header.split(',');
-    const set = new Set();
-    const languages = [];
-    for (let order = 0; order < parts.length; order++) {
-        const language = parts[order].substr(0, 2);
-        if (language === except || set.has(language)) continue;
-        set.add(language);
-        languages.push({language, order});
-    }
-    return languages;
-}
-
 const setBody = {
     assign: 'body',
     failAction: 'error',
@@ -830,16 +817,14 @@ module.exports = async function create({id, socket, channel, logLevel, logger, m
                 if (result && typeof result.httpResponse === 'function') applyMeta(response, {httpResponse: result.httpResponse()});
                 return applyMeta(response, $meta);
             } catch (error) {
-                if (socket.translateErrors && error.type) {
+                if (Array.isArray(socket.translateErrors) && socket.translateErrors.length && error.type) {
                     const $meta = params[params.length - 1];
-                    const acceptLanguage = $meta?.httpRequest?.headers['accept-language'] || $meta?.language?.iso2Code;
-                    const languageList = acceptLanguage && parseAcceptLanguageHeader(acceptLanguage, 'en');
-
-                    if (languageList?.length) {
+                    const language = ($meta?.httpRequest?.headers['accept-language']?.substr(0, 2) || $meta?.language?.iso2Code)?.toLowerCase();
+                    if (socket.translateErrors.includes(language)) {
                         const [{translation}] = await brokerRequest(
-                            {itemCode: error.type, languageList},
+                            {itemCode: error.type, language},
                             {...$meta, method: 'core.translation.errorFetch'}
-                        ).catch(err => logger.error(Error(`Could not translate error: ${err.message}`)));
+                        ).catch(err => { logger.error(Error(`Could not translate error: ${err.message}`)); });
                         if (translation) {
                             errors.translateError(error, translation.itemNameTranslation);
                             logger.error(error);
